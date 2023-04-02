9fd19d3450372766745132a29666a1b49985306d
Matt Rossman <22670878+mattrossman@users.noreply.github.com>
2023-04-01 22:42:48 -0400

    Update three.js template

--- src/jsfiddle/three.ts
+++ src/jsfiddle/three.ts
@@ -25,106 +25,100 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GUI } from 'lil-gui'
 
-async function main() {
+// Scene
 
-	// Scene
-
-	const scene = new THREE.Scene()
-	scene.background = new THREE.Color('black')
+const scene = new THREE.Scene()
+scene.background = new THREE.Color('black')
 
 
-	// Camera
+// Camera
 
-	const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-	camera.position.set(1, 2, 3)
-
-
-	// WebGL renderer
-
-	const renderer = new THREE.WebGLRenderer({ antialias: true })
-	document.body.appendChild(renderer.domElement)
+const camera = new THREE.PerspectiveCamera(75, undefined, 0.1, 1000)
+camera.position.set(1, 2, 3)
 
 
-	// Lights
+// WebGL renderer
 
-	const directionalLight = new THREE.DirectionalLight('white')
-	directionalLight.position.set(2, 3, 4)
-	scene.add(directionalLight)
-
-	const ambientLight = new THREE.AmbientLight('white', 0.3)
-	scene.add(ambientLight)
+const renderer = new THREE.WebGLRenderer({ antialias: true })
+document.body.appendChild(renderer.domElement)
 
 
-	// OrbitControls
+// Lights
 
-	const controls = new OrbitControls(camera, renderer.domElement)
+const directionalLight = new THREE.DirectionalLight('white')
+directionalLight.position.set(2, 3, 4)
+scene.add(directionalLight)
 
-
-	// Grid
-
-	const grid = new THREE.GridHelper()
-	grid.position.y = -1
-	scene.add(grid)
+const ambientLight = new THREE.AmbientLight('white', 0.3)
+scene.add(ambientLight)
 
 
-	// Mesh
+// OrbitControls
 
-	const geometry = new THREE.BoxGeometry()
-	const material = new THREE.MeshStandardMaterial({ color: 'palegreen' })
-	const mesh = new THREE.Mesh(geometry, material)
-
-	scene.add(mesh)
-
-	
-	// GUI
-	
-	const params = {}
-	const gui = new GUI()
-	
-	params.speed = 1
-	gui.add(params, 'speed').step(0.1).min(0).max(2)
+const controls = new OrbitControls(camera, renderer.domElement)
 
 
-	// Render loop
+// Grid
 
-	const clock = new THREE.Clock()
-
-	let prevElapsedTime = 0
-
-	function render() {
-
-		const elapsedTime = clock.getElapsedTime()
-		const dt = elapsedTime - prevElapsedTime
-		prevElapsedTime = elapsedTime
-
-		controls.update()
-
-		mesh.rotation.x = mesh.rotation.y += dt * params.speed
-
-		renderer.render(scene, camera)
-
-	}
-
-	renderer.setAnimationLoop(render)
+const grid = new THREE.GridHelper()
+grid.position.y = -1
+scene.add(grid)
 
 
-	// Window sizing
+// Mesh
 
-	function onWindowResize() {
+const geometry = new THREE.BoxGeometry()
+const material = new THREE.MeshStandardMaterial({ color: 'palegreen' })
+const mesh = new THREE.Mesh(geometry, material)
 
-		camera.aspect = window.innerWidth / window.innerHeight
-		camera.updateProjectionMatrix()
-		renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+scene.add(mesh)
 
-		renderer.setSize(window.innerWidth, window.innerHeight)
 
-	}
+// GUI
 
-	window.addEventListener('resize', onWindowResize, false)
+const params = {}
+const gui = new GUI()
 
-	onWindowResize() // Run once to initialize
+params.speed = 1
+gui.add(params, 'speed').step(0.1).min(0).max(2)
+
+
+// Render loop
+
+const clock = new THREE.Clock()
+
+let prevElapsedTime = 0
+
+function render() {
+
+	const elapsedTime = clock.getElapsedTime()
+	const dt = elapsedTime - prevElapsedTime
+	prevElapsedTime = elapsedTime
+
+	controls.update()
+
+	mesh.rotation.x = mesh.rotation.y += dt * params.speed
+
+	renderer.render(scene, camera)
 
 }
 
-main()
+renderer.setAnimationLoop(render)
+
+
+// Window sizing
+
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+	renderer.setSize(window.innerWidth, window.innerHeight)
+
+}
+
+window.addEventListener('resize', onWindowResize, false)
+
+onWindowResize() // Run once to initialize
 `
