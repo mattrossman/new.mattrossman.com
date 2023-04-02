67f4833711f08884f2754634a88466d1c173314e
Matt Rossman <22670878+mattrossman@users.noreply.github.com>
2023-04-01 22:03:38 -0400

    Update card styles

--- astro.config.js
+++ astro.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config'
-
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
+	experimental: {
+		assets: true,
+	},
+	image: {
+		service: 'astro/assets/services/sharp',
+	},
 })
